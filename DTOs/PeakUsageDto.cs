4f09277aefa2631a7c6eef9f33fd5bb0a7202a31
Uday1908 <Uday.Kiran.S@gds.ey.com>
2026-01-20 10:13:32 +0530

    Dev5 clss

---
+++ DTOs/PeakUsageDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Facility_Management.DTOs
+{
+    public class PeakUsageDto
+    {
+        public string ResourceName { get; set; }
+        public int PeakHour { get; set; }
+        public int BookingCount { get; set; }
+    }
+}
