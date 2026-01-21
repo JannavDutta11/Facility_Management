3eceb6308afe48d1fa1dba1ce547a1d0147a99eb
JannavDutta11 <Jannav.Dutta@gds.ey.com>
2026-01-21 14:17:20 +0530

    c

--- Controllers/BookingController.cs
+++ Controllers/BookingController.cs
@@ -3,6 +3,7 @@ using Facility_Management.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Data;
 
 
 
@@ -28,7 +29,17 @@ namespace Facility_Management.Controllers
                 if (dto.StartTime >= dto.EndTime)
                     return BadRequest("StartTime must be before EndTime.");
 
-                bool conflict = await _context.Bookings.AnyAsync(b =>
+                if(dto.NumberOfUsers <=0)
+                return BadRequest("Number of users must be greater than zero");
+
+            bool resourceExists = await _context.Resource.AnyAsync(r => r.ResourceId == dto.ResourceId);
+
+            if (!resourceExists)
+                return BadRequest("Invalid ResourceId. Resource does not exist.");
+
+
+
+            bool conflict = await _context.Bookings.AnyAsync(b =>
                     b.ResourceId == dto.ResourceId &&
                     b.Status == "Approved" &&
                     dto.StartTime < b.EndTime &&
@@ -38,26 +49,29 @@ namespace Facility_Management.Controllers
                 if (conflict)
                     return BadRequest("Booking conflict detected.");
 
-                Booking booking = new Booking
-                {
-                    ResourceId = dto.ResourceId,
-                    StartTime = dto.StartTime,
-                    EndTime = dto.EndTime,
-                    Purpose = dto.Purpose,
-                    NumberOfUsers = dto.NumberOfUsers,
-                    Status = "Pending",
-                    CreatedAt = DateTime.Now
-                };
+        
 
-                await _context.Bookings.AddAsync(booking);
-                await _context.SaveChangesAsync();
 
-                return Ok(booking);
-            }
+            //Booking booking = new Booking
+            //{
+            //    ResourceId = dto.ResourceId,
+            //    StartTime = dto.StartTime,
+            //    EndTime = dto.EndTime,
+            //    Purpose = dto.Purpose,
+            //    NumberOfUsers = dto.NumberOfUsers,
+            //    Status = "Pending",
+            //    CreatedAt = DateTime.Now
+            //};
 
-            // APPROVE BOOKING
-           
-            [HttpPut("approve/{id}")]
+            //await _context.Bookings.AddAsync(booking);
+            //await _context.SaveChangesAsync();
+
+            //return Ok(booking);
+        }
+
+        // APPROVE BOOKING
+
+        [HttpPut("approve/{id}")]
             public async Task<IActionResult> ApproveBooking(int id)
             {
                 var booking = await _context.Bookings.FindAsync(id);
